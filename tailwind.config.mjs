27b4d7120d5031a63a1deb4674c63ebf92fc03b8
Mario Pabon <me@mariopabon.com>
2025-02-22 14:33:39 -0500

    Revert "setup admin"
    This reverts commit 8add5953d0d45138fef8807abce11398ee68ae16.

--- tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const tailwindConfig = {
-  content: ['./node_modules/@premieroctet/next-admin/dist/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
-  presets: [require('@premieroctet/next-admin/preset')],
-}
-
-export default tailwindConfig
