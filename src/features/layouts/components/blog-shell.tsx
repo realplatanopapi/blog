acf036ce6aba4ccf5b7b3239c7bfd9b230e4ba82
Mario Pabon <me@mariopabon.com>
2025-02-22 23:26:57 -0500

    Add copyright to footer

--- src/features/layouts/components/blog-shell.tsx
+++ src/features/layouts/components/blog-shell.tsx
@@ -1,11 +1,25 @@
 import { PropsWithChildren } from 'react'
 
-import { Container } from '@/components'
+import { Box, Container, Text, TextLink } from '@/components'
 
 export function BlogShell({ children }: PropsWithChildren) {
   return (
     <Container as="main" maxWidth="prose" minHeight="calc(100vh + 1px)" py={6} px={4}>
       {children}
+      <Box pt={6}>
+        <Text color="fg.muted" fontSize="sm">
+          <TextLink
+            color="inherit"
+            href={{
+              pathname: '/',
+            }}
+            variant="plain"
+          >
+            coquito.io
+          </TextLink>{' '}
+          © {new Date().getFullYear()}
+        </Text>
+      </Box>
     </Container>
   )
 }
