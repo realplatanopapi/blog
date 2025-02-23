ac23b3b3be5e36209a8a850a67ccf49144aed688
Mario Pabon <me@mariopabon.com>
2025-02-23 01:07:27 -0500

    Fix metadata

--- src/app/(blog)/p/[slug]/page.tsx
+++ src/app/(blog)/p/[slug]/page.tsx
@@ -1,9 +1,26 @@
+import { Metadata } from 'next'
+
 import { Content } from '@/components/editor/types'
 import { PostView } from '@/features/posts/views/post-view'
 import { dbClient } from '@/lib/db'
 
 interface PostPageProps {
   params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+  const { slug } = await params
+
+  const post = await dbClient.post.findFirstOrThrow({
+    where: {
+      slug,
+      published: true,
+    },
+  })
+
+  return {
+    title: post.title,
+  }
 }
 
 export default async function PostPage({ params }: PostPageProps) {
