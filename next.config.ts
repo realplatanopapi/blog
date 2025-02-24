82ca993ce2e03791eec5cb1c52338a7cdbbd6371
Mario Pabon <me@mariopabon.com>
2025-02-23 21:09:07 -0500

    ignore ts and es during builds

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     optimizePackageImports: ['@chakra-ui/react', '@emotion/react', 'next-themes', 'react-icons'],
     typedRoutes: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
