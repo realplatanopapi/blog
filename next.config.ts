9c0a661f5b89555a45ec676406422ce082245dfb
Mario Pabon <me@mariopabon.com>
2025-02-23 23:18:57 -0500

    setup bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,25 @@
+import pluginBundleAnalyzer from '@next/bundle-analyzer'
 import type { NextConfig } from 'next'
+
+const withBundleAnalyzer = pluginBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    optimizePackageImports: ['@chakra-ui/react', '@emotion/react', 'next-themes', 'react-icons'],
+    optimizePackageImports: [
+      '@ark-ui/react',
+      '@chakra-ui/react',
+      '@emotion/react',
+      '@premieroctet/next-admin',
+      'highlight.js',
+      'next-themes',
+      'react-highlight',
+      'react-icons',
+    ],
     typedRoutes: true,
   },
   output: 'standalone',
@@ -14,4 +28,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withBundleAnalyzer(nextConfig)
