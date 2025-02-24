42009fea39892aabfd0b656fa68a6ca1cd6e0a6c
Mario Pabon <me@mariopabon.com>
2025-02-24 18:37:04 -0500

    Skip linting during build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     optimizePackageImports: ['@chakra-ui/react', '@emotion/react', 'next-themes', 'react-icons'],
     typedRoutes: true,
   },
   output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 export default nextConfig
