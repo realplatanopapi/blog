f55cfe2e29b121816ebbc64794a95d5d55d04c17
Mario Pabon <me@mariopabon.com>
2025-02-27 23:30:32 -0500

    lock down api

--- src/app/api/admin/[[...nextadmin]]/route.ts
+++ src/app/api/admin/[[...nextadmin]]/route.ts
@@ -1,11 +1,19 @@
 import { createHandler } from '@premieroctet/next-admin/appHandler'
+import { NextResponse } from 'next/server'
 
 import { adminOptions } from '@/features/admin'
+import { authorize } from '@/lib/auth/authorize'
 import { dbClient, PlainDBClient } from '@/lib/db'
 
 const { run } = createHandler({
   apiBasePath: '/api/admin',
   prisma: dbClient as unknown as PlainDBClient,
+  onRequest: async () => {
+    const session = await authorize()
+    if (session) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+  },
   options: adminOptions,
 })
 
