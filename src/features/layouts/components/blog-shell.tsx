10c61ae6bf06f1239ff28acb7813ab38a63e83a8
Mario Pabon <me@mariopabon.com>
2025-03-01 13:20:16 -0500

    Fill screen

--- src/features/layouts/components/blog-shell.tsx
+++ src/features/layouts/components/blog-shell.tsx
@@ -4,9 +4,19 @@ import { Container, Flex, Text, TextLink } from '@/components'
 
 export function BlogShell({ children }: PropsWithChildren) {
   return (
-    <Container as="main" maxWidth="prose" minHeight="calc(100vh + 1px)" py={6} px={4}>
-      {children}
-      <Flex justifyContent="space-between" pt={8}>
+    <Container
+      as="main"
+      display="flex"
+      flexDirection="column"
+      maxWidth="prose"
+      minHeight="calc(100vh + 1px)"
+      py={6}
+      px={4}
+    >
+      <Flex flex="1 0 100%" flexDirection="column">
+        {children}
+      </Flex>
+      <Flex flex="0 0 auto" justifyContent="space-between" pt={8}>
         <Text color="fg.muted" fontSize="sm">
           <TextLink
             color="inherit"
