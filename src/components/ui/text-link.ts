e26a632e9144de8275195459aac4a3bf5c4444c1
Mario Pabon <me@mariopabon.com>
2025-03-01 23:53:11 -0500

    missing types

--- src/components/ui/text-link.ts
+++ src/components/ui/text-link.ts
@@ -1,10 +1,10 @@
 'use client'
 
-import { chakra } from '@chakra-ui/react'
-import Link, { LinkProps } from 'next/link'
+import { chakra, LinkProps } from '@chakra-ui/react'
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 
 import { linkRecipe } from '@/theme/recipes/link'
 
-export interface TextLinkProps<RouteType> extends LinkProps<RouteType> {}
+export type TextLinkProps<RouteType> = LinkProps & NextLinkProps<RouteType>
 
-export const TextLink = chakra(Link, linkRecipe)
+export const TextLink = chakra(NextLink, linkRecipe)
