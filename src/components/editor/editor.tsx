abfba94f2e18d0ac32ce74853ca729e1c657db13
Mario Pabon <me@mariopabon.com>
2025-03-05 21:23:45 -0500

    add floating menu

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -3,8 +3,9 @@
 import './editor.css'
 
 import Placeholder from '@tiptap/extension-placeholder'
-import { EditorContent, useEditor, UseEditorOptions } from '@tiptap/react'
+import { BubbleMenu, EditorContent, useEditor, UseEditorOptions } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import cx from 'classix'
 
 import { Content } from './types'
 
@@ -38,5 +39,33 @@ export function Editor({ content = {}, editable = true, onUpdate }: EditorProps)
     },
   })
 
-  return <EditorContent className="w-full py-5" editor={editor} />
+  return (
+    <>
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div className="bubble-menu">
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={cx(editor.isActive('bold') && 'is-active')}
+            >
+              Bold
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={cx(editor.isActive('italic') && 'is-active')}
+            >
+              Italic
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={cx(editor.isActive('strike') && 'is-active')}
+            >
+              Strike
+            </button>
+          </div>
+        </BubbleMenu>
+      )}
+      <EditorContent className="w-full py-5" editor={editor} />
+    </>
+  )
 }
