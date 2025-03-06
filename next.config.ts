fabfd9ae0c070bf8a04db9eea02f98a662f308e8
Mario Pabon <me@mariopabon.com>
2025-03-05 21:11:37 -0500

    optimize tiptap import

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    optimizePackageImports: ['@chakra-ui/react', '@emotion/react', 'next-themes', 'react-icons'],
+    optimizePackageImports: ['@chakra-ui/react', '@emotion/react', 'next-themes', 'react-icons', '@tiptap/react'],
   },
   output: 'standalone',
   typescript: {
